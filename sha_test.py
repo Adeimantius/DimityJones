6658754e0b945c7257df35bd8a83889008c108c5
Markus Ristich <markusristich@gmail.com>
2025-05-25 16:22:13 -0700

    Sha Puzzle 3 passing

--- sha_test.py
+++ sha_test.py
@@ -26,8 +26,8 @@ def compare_sha256_hashes(data, expected_hash):
 #test_buffer = "To begin with, for example, and to make sure your SHA-256 hash function is working, the hash value or checksum of this sentence, from capital 'T' to concluding colon, expressed in hexadecimal, is:"
 #input_text = test_buffer
 #target_sha = "10c0c7d9b0222a5a61601337105f1cbb7b1723b991404b870537095d1174f2b2"
-filepath = r"D:\dev\puzzles\solution-2.txt"
-target_sha = "d5692933375a798f0916f2556233329d3c45458a80dac272560d6c4c981f79a1"
+filepath = r"D:\dev\puzzles\DimityJones\solution-3.txt"
+target_sha = "2f2c81c6f85a957ec494899f0e1ca52a3d4957aa5f71ed115db48657e62742bc"
 input_text = load_text(filepath)
 
 match = compare_sha256_hashes(input_text, target_sha)
